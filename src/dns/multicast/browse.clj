d4d893c6765f09b6ad363238a929d7f5bc2c0497
Mircea Toma <mircea.toma@icesoft.com>
2024-01-10 20:52:22 +0200

    return results as lazy sequence

--- src/dns/multicast/browse.clj
+++ src/dns/multicast/browse.clj
@@ -1,42 +1,47 @@
 (ns dns.multicast.browse
   (:require
+    [clojure.core.async :as async :refer [<!! >!! <! >!]]
     [socket.io.udp :refer [socket]]
     [dns.multicast.message :refer [encode-srv-query-message]]
     [clj-commons.byte-streams :refer [to-string print-bytes]]
     [taoensso.timbre :refer [debug info warn error]]))
 
 (defmacro on-term-signal [& handler]
-  `(.addShutdownHook (Runtime/getRuntime)
-                     (Thread. (fn []
-                                (debug "sigterm captured")
-                                ~@handler))))
+  `(.addShutdownHook
+     (Runtime/getRuntime)
+     (Thread. ^Runnable
+              (fn []
+                (debug "sigterm captured")
+                ~@handler))))
 
 (def mdns-port 5353)
 (def multicast-host "224.0.0.251")
+
+(defn results [messages]
+  (lazy-seq (cons (<!! messages) (results messages))))
 
 (defn browse [protocol type & subtypes]
   (debug "starting browser ...")
   (debug "listening for mdns response ...")
 
   (let [message-bytes (encode-srv-query-message protocol type subtypes)
-
+        received-messages (async/chan)
         {send :send close :close}
         (socket multicast-host mdns-port
-                (fn [host port message]
-                  (println "received [" host ":" port "] ------------")
-                  (print-bytes message)))]
-
-    (on-term-signal
-      (info "stopping browser...")
-      (close)
-      (shutdown-agents)
-      (info "stopped browser"))
+                (fn [& parameters] (>!! received-messages parameters)))]
 
     (future
       (Thread/sleep 5000)
       (debug "sending mdns request")
       (send multicast-host mdns-port message-bytes)
-      (debug "sent mdns request"))))
+      (debug "sent mdns request"))
+
+    (results received-messages)))
 
 (defn -main [& args]
-  (browse "udp" "sleep-proxy"))
+  (run!
+    (fn [[host port message]]
+      (println "received [" host ":" port "] ------------")
+      (print-bytes message))
+    (browse "udp" "sleep-proxy"))
+  (shutdown-agents))
