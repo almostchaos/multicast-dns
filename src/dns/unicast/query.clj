9a2ee7557dc77cb790ae46e826f6e90ae6eff389
Mircea Toma <mircea.toma@icesoft.com>
2024-01-21 15:07:45 +0200

    refactor

--- src/dns/unicast/query.clj
+++ src/dns/unicast/query.clj
@@ -6,28 +6,27 @@
     [socket.io.udp :refer [socket]]
     [taoensso.timbre :refer [debug]]))
 
-(defn query
-  [name]
+(defn name->ip [dns name]
   (debug "starting client ...")
   (debug "listening for dns response ...")
 
   (let [message-bytes (time (a-query name))
         messages (async/timeout 500)
-        {send :send close :close}
+        {send :send close-socket :close}
         ;;use all network interfaces and pick a random port
         (socket "0.0.0.0" 0 (fn [& parameters] (>!! messages parameters)))]
 
     (debug "sending dns request")
-    (send "8.8.8.8" 53 message-bytes)
+    (send dns 53 message-bytes)
     (debug "sent dns request")
 
     (let [result (<!! messages)]
-      (close)
+      (close-socket)
       result)))
 
 
 (defn -main [& args]
-  (let [[host port message] (time (query "wikipedia.org"))]
+  (let [[host port message] (time (name->ip "8.8.8.8" "wikipedia.org"))]
     (println "received [" host ":" port "] ------------")
     (clojure.pprint/pprint (decode-message message)))
 
