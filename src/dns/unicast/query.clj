1aad475381b6384624dd8b2424767745f80c79bd
Mircea Toma <mircea.toma@icesoft.com>
2024-01-22 14:20:09 +0200

    add simpler function signature

--- src/dns/unicast/query.clj
+++ src/dns/unicast/query.clj
@@ -6,33 +6,36 @@
     [socket.io.udp :refer [socket]]
     [taoensso.timbre :refer [debug]]))
 
-(defn name->ip [dns name]
-  (debug "starting client ...")
-  (debug "listening for dns response ...")
+(defn name->ip
+  ([name]
+   (name->ip name "8.8.8.8"))
+  ([name dns]
+   (debug "starting client ...")
+   (debug "listening for dns response ...")
 
-  (let [message-bytes (a-query name)
-        messages (async/timeout 500)
-        {send :send close-socket :close}
-        ;;use all network interfaces and pick a random port
-        (socket "0.0.0.0" 0 (fn [& parameters] (>!! messages parameters)))]
+   (let [message-bytes (a-query name)
+         messages (async/timeout 500)
+         {send :send close-socket :close}
+         ;;use all network interfaces and pick a random port
+         (socket "0.0.0.0" 0 (fn [& parameters] (>!! messages parameters)))]
 
-    (debug "sending dns request")
-    (send dns 53 message-bytes)
-    (debug "sent dns request")
+     (debug "sending dns request")
+     (send dns 53 message-bytes)
+     (debug "sent dns request")
 
-    (let [[host port message] (<!! messages)]
-      (close-socket)
-      (->
-        (->>
-          (decode-message message)
-          (filter
-            (fn [section]
-              (= type:A (:TYPE section)))))
-        (first)
-        (:RDATA)))))
+     (let [[host port message] (<!! messages)]
+       (close-socket)
+       (->
+         (->>
+           (decode-message message)
+           (filter
+             (fn [section]
+               (= type:A (:TYPE section)))))
+         (first)
+         (:RDATA))))))
 
 
 (defn -main [& args]
-  (println (name->ip "8.8.8.8" "wikipedia.org"))
+  (println (name->ip "wikipedia.org"))
 
   (shutdown-agents))
